cc28a5d76ed5039effcd18ede68ae7d22b5f550e
arvind4403 <kr.arvind027@gmail.com>
2023-07-10 20:24:08 +0530

    New code is here

---
+++ Account.java
@@ -0,0 +1,30 @@
+public class BankAccount {
+   private int AccountNo;
+   private double AccountBalance;
+    gitprivate String AccountHolderName;
+
+   public BankAccount(int accountNo, double accountBalance, String AccountHolderName) {
+       this.AccountNo = accountNo;
+       this.AccountBalance = accountBalance;
+       this.AccountHolderName = AccountHolderName;
+   }
+   public void deposit(double amount){
+       AccountBalance+=amount;
+   }
+   public void withdraw(double amount){
+       AccountBalance-=amount;
+   }
+   public void displayAccountInfo(){
+       System.out.println("Account number : "+AccountNo);
+       System.out.println("Account Holder Name : "+AccountHolderName);
+       System.out.println("Account Balance :"+ AccountBalance);
+
+   }
+   public static void main(String[] args){
+       BankAccount Account = new BankAccount(1234,50000,"Arvind");
+       Account.deposit(15);
+       Account.withdraw(16);
+       Account.displayAccountInfo();
+   }
+
+}
